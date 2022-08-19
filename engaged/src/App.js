4f5592be6c8d7f9f5dcea349cac27a8a40094105
ShaunR23 <shaunradford23@gmail.com>
2022-08-19 16:14:11 -0400

    installed sentry

--- engaged/src/App.js
+++ engaged/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import React, {useState, useEffect } from "react";
+import * as Sentry from "@sentry/react";
+import "./App.css";
+  
 function App() {
+const [hello, setHello] = useState("");
+useEffect (() => {
+  const callBackendAPI = async () => {
+    const response = await fetch('http://localhost:3001');
+    const data = await response.json();
+    setHello(data)
+    if (!response.ok) {
+      throw new Error("Network response was not OK!");
+    }
+  
+  }
+}, [])
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{!hello ? "Loading..." : hello}</p>
       </header>
     </div>
   );
 }
-
 export default App;
--- engaged/src/index.js
+++ engaged/src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+Sentry.init({
+  dsn: "https://f827f8a2a6c2465fa4c104fcb0643bd0@o1368148.ingest.sentry.io/6670643",
+  integrations: [new Integrations.BrowserTracing()],
+
+  tracesSampleRate: 1.0,
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
