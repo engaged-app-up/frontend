aa64deb212b27d3e93c092091badbf59e7c7e8ab
itrytocodethings <wbaileydev@gmail.com>
2022-09-04 15:15:44 -0400

    Started on login state logic to get started with firebase. Removed separate 'Register' component. Handling signup/signin with single component

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,17 +1,28 @@
-import React, {useContext} from "react";
+import React, { useContext, useState } from "react";
 import { GlobalContext } from "../Context/GlobalContext/GlobalContext";
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
 export default function Login() {
-  const globalState = useContext(GlobalContext);
-  const auth = getAuth();
+  const [isLogin, setIsLogin] = useState(true);
+  const [userInput, setUserInput] = useState({email: '', password: ''}); //initial state for form inputs.
+  console.log(userInput);
+  const globalState = useContext(GlobalContext); //global context
+  const auth = getAuth(); //firebase auth obj
 
-  console.log(auth);
+  const handleInput = (event) => {
+    setUserInput({ ...userInput, [event.target.id]: event.target.value });
+  }
+
+  const handleFormState = (event) => {
+    setUserInput({email: '', password: ''});
+    setIsLogin(!isLogin);
+  }
+
   return (
     <div className="relative flex flex-col justify-center min-h-screen overflow-hidden c">
       <div className="w-full p-6 m-auto bg-blue-400 rounded-md shadow-md lg:max-w-xl ">
         <h1 className="text-3xl font-semibold text-center text-yellow-400 underline">
-          Sign in
+          {isLogin ? 'Sign In' : 'Sign Up'}
         </h1>
         <form className="mt-6">
           <div className="mb-2">
@@ -22,8 +33,11 @@ export default function Login() {
               Email
             </label>
             <input
+              id="email"
               type="email"
               className="block w-full px-4 py-2 mt-2  bg-white border rounded-md focus:border-blue-400 focus:ring-yellow-400 focus:outline-none focus:ring "
+              onChange={handleInput}
+              value={userInput.email}
             />
           </div>
           <div className="mb-2">
@@ -34,25 +48,64 @@ export default function Login() {
               Password
             </label>
             <input
+              id="password"
+              name="password"
               type="password"
               className="block w-full px-4 py-2 mt-2  bg-white border rounded-md focus:border-blue-400 focus:ring-yellow-400 focus:outline-none focus:ring "
+              onChange={handleInput}
+              value={userInput.password}
             />
           </div>
-          <a href="#" className="text-xs text-yellow-400 hover:underline">
+          {isLogin && <a href="#" className="text-xs text-yellow-400 hover:underline">
             Forget Password?
-          </a>
+          </a>}
+          {!isLogin && (
+            <>
+              <div className="mb-2">
+                <label
+                  htmlFor="firstName"
+                  className="block text-sm font-semibold text-yellow-400"
+                >
+                  First Name
+                </label>
+                <input
+                  id="firstName"
+                  name="firstName"
+                  type="text"
+                  className="block w-full px-4 py-2 mt-2  bg-white border rounded-md focus:border-blue-400 focus:ring-yellow-400 focus:outline-none focus:ring "
+                  onChange={handleInput}
+                  value={userInput.firstName}
+                />
+              </div>
+              <div className="mb-2">
+                <label
+                  htmlFor="lastName"
+                  className="block text-sm font-semibold text-yellow-400"
+                >
+                  Last Name
+                </label>
+                <input
+                  id="lastName"
+                  name="lastName"
+                  type="text"
+                  className="block w-full px-4 py-2 mt-2  bg-white border rounded-md focus:border-blue-400 focus:ring-yellow-400 focus:outline-none focus:ring "
+                  onChange={handleInput}
+                  value={userInput.lastName}
+                />
+              </div>
+            </>
+          )}
           <div className="mt-6">
             <button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-yellow-400 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600">
-              Login
+              {isLogin ? "Login" : "Signup"}
             </button>
           </div>
         </form>
 
         <p className="mt-8 text-xs font-light text-center text-gray-700">
-          {" "}
-          Don't have an account?{" "}
-          <a href="#" className="font-medium text-yellow-400 hover:underline">
-            Sign up
+          {isLogin ? "Don't have an account?" : "Already have an account"}
+          <a href="#" className="font-medium text-yellow-400 hover:underline" onClick={handleFormState}>
+            {isLogin ? "Sign up" : "Login"}
           </a>
         </p>
       </div>
