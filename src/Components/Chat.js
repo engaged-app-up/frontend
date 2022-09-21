975c27186da18cc1c1521a7f6852335aedbe467e
ShaunR23 <shaunradford23@gmail.com>
2022-09-21 16:58:31 -0400

    temporary chat design

--- src/Components/Chat.js
+++ src/Components/Chat.js
@@ -1,13 +1,10 @@
 import { useEffect, useState } from "react";
 
-
-
-
-export default function Chat({socket, username, room}) {
+export default function Chat({ socket, username, room }) {
   // Messages States
   const [currentMessage, setCurrentMessage] = useState("");
   const [messageReceived, setMessageReceived] = useState("");
-  const [messageList, setMessageList] = useState([])
+  const [messageList, setMessageList] = useState([]);
 
   const sendMessage = async () => {
     if (currentMessage !== "") {
@@ -29,52 +26,56 @@ export default function Chat({socket, username, room}) {
 
   useEffect(() => {
     socket.on("receive_message", (data) => {
-    setMessageList((list) => [...list, data]);
+      setMessageList((list) => [...list, data]);
     });
   }, [socket]);
   return (
-    <div className="chat-window">
-      <div className="chat-header">
-        <p>Live Chat</p>
-      </div>
-      <div className="chat-body">
-        
-          {messageList.map((messageContent) => {
-            return (
-              <div
-                className="message"
-                id={username === messageContent.author ? "you" : "other"}
-              >
-                <div>
-                  <div className="message-content">
-                    <p>{messageContent.message}</p>
+    <body class="flex flex-col items-center justify-center w-screen min-h-screen bg-gray-100 text-gray-800 p-10">
+      <div class="flex flex-col flex-grow w-full max-w-xl bg-white shadow-xl rounded-lg overflow-hidden">
+        <div class="flex flex-col flex-grow h-0 p-4 overflow-auto">
+          <div class="flex w-full mt-2 space-x-3 max-w-xs">
+            <div class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300"></div>
+            <div rounded-full bg-gray-300>
+              {messageList.map((messageContent) => {
+                return (
+                  <div
+                    className="message "
+                    id={username === messageContent.author ? "you" : "other"}
+                  >
+                    <div>
+                      <div className="message-content">
+                        <p>{messageContent.message}</p>
+                      </div>
+
+                      <p id="time">{messageContent.time}</p>
+                      <p id="author">{messageContent.author}</p>
+                    </div>
                   </div>
-                  <div className="message-meta">
-                    <p id="time">{messageContent.time}</p>
-                    <p id="author">{messageContent.author}</p>
-                  </div>
-                </div>
-              </div>
-            );
-          })}
-       
+                );
+              })}
+            </div>
+          </div>
+
+          <div class="flex w-full mt-2 space-x-3 max-w-xs"></div>
+        </div>
+
+        <div class="bg-gray-300 p-4">
+          <input
+            type="text"
+            value={currentMessage}
+            placeholder="Hey..."
+            onChange={(event) => {
+              setCurrentMessage(event.target.value);
+            }}
+            onKeyPress={(event) => {
+              event.key === "Enter" && sendMessage();
+            }}
+          />
+          <button class="pl-5" onClick={sendMessage}>
+            Send
+          </button>
+        </div>
       </div>
-      <div className="chat-footer">
-        <input
-          type="text"
-          value={currentMessage}
-          placeholder="Hey..."
-          onChange={(event) => {
-            setCurrentMessage(event.target.value);
-          }}
-          onKeyPress={(event) => {
-            event.key === "Enter" && sendMessage();
-          }}
-        />
-        <button onClick={sendMessage}>&#9658;</button>
-      </div>
-    </div>
+    </body>
   );
 }
-
-
